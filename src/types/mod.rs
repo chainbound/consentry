ba704b8973d5cf45bb32f4f1f5739c11f3bb54a7
Jonas Bostoen <jonas.bostoen@hotmail.com>
2022-12-23 14:10:56 +0100

    fix: mod.rs

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -5,11 +5,6 @@ mod subnet;
 mod sync_state;
 mod topics;
 
-pub use types::{
-    BeaconBlock, BitVector, EthSpec, ExecutionPayload, FullPayload, MainnetEthSpec,
-    SignedBeaconBlock,
-};
-
 pub type EnrAttestationBitfield<T> = BitVector<<T as EthSpec>::SubnetBitfieldLength>;
 pub type EnrSyncCommitteeBitfield<T> = BitVector<<T as EthSpec>::SyncCommitteeSubnetCount>;
 
@@ -17,6 +12,8 @@ pub type Enr = discv5::enr::Enr<discv5::enr::CombinedKey>;
 
 pub use globals::NetworkGlobals;
 pub use pubsub::{PubsubMessage, SnappyTransform};
+use ssz_types::BitVector;
 pub use subnet::{Subnet, SubnetDiscovery};
 pub use sync_state::{BackFillState, SyncState};
 pub use topics::{subnet_from_topic_hash, GossipEncoding, GossipKind, GossipTopic};
+use types::EthSpec;
